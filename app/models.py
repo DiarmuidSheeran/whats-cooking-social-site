2b7d09b65c42dc5f86c89d702192156843bb96a1
Diarmuid Sheeran <dssheeran@outlook.com>
2023-10-20 17:42:29 +0000

    Add more items to class object userprofile

--- app/models.py
+++ app/models.py
@@ -8,6 +8,21 @@ class UserProfile(models.Model):
     fname = models.CharField(max_length=200, null=True)
     lname = models.CharField(max_length=200, null=True)
     profile_pic = CloudinaryField('image', default='placeholder')
+    followers = models.ManyToManyField(User, related_name='following_users', blank=True)
+    following = models.ManyToManyField(User, related_name='followers', blank=True)
+    bio = models.TextField(blank=True)
+    twitter = models.URLField(blank=True)
+    instagram = models.URLField(blank=True)
+    facebook = models.URLField(blank=True)
 
     def __str__(self):
         return f"{self.fname} {self.lname}"
+
+     def follow(self, user_to_follow):
+        self.following.add(user_to_follow)
+
+    def unfollow(self, user_to_unfollow):
+        self.following.remove(user_to_unfollow)
+
+    def follower(self, followers):
+        self.followers.add(followers)
