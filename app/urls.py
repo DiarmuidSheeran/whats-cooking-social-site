724573ef0d4f8e69a88ab3cbcf1928f8ca335769
Diarmuid Sheeran <dssheeran@outlook.com>
2023-10-21 09:59:16 +0000

    Add Url paths for signup and login

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.landing, name="landing"),
+    path('signup/', views.signup , name="signup"),
+    path('login/', views.user_login, name="login"),
 ]
