6ef7c7e2189739c5c3f7d491ed73477469dae578
Diarmuid Sheeran <dssheeran@outlook.com>
2023-10-21 09:45:50 +0000

    Create View with form for signup template

--- app/views.py
+++ app/views.py
@@ -6,3 +6,20 @@ from .decorators import unauthenticated_user
 @unauthenticated_user
 def landing(request):
     return render(request, 'landing.html')
+
+@unauthenticated_user
+def signup(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            fname = form.cleaned_data.get('fname')
+            lname = form.cleaned_data.get('lname')
+            UserProfile.objects.create(user=user, fname=fname, lname=lname, profile_pic="placeholder")
+            return redirect('login')
+
+    context = {'form': form}
+    return render(request, 'signup.html', context)
+
