918f8c261646dea308b7b1537d23488612f8662e
Diarmuid Sheeran <dssheeran@outlook.com>
2023-10-23 10:48:58 +0000

    Create post creation form

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
-from .models import UserProfile
+from .models import UserProfile, Post
 
 class CreateUserForm(UserCreationForm):
     fname = forms.CharField(max_length=200, required=True)
@@ -10,3 +10,8 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2', 'fname', 'lname']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'featured_image']
