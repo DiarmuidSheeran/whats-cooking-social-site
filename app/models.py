3d0bc58087ad6ee6378a02778ae4a1833b3f729a
Diarmuid Sheeran <dssheeran@outlook.com>
2023-10-27 11:22:40 +0000

    Change post title being unique to false

--- app/models.py
+++ app/models.py
@@ -29,7 +29,7 @@ class UserProfile(models.Model):
         self.followers.add(followers)
 
 class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
+    title = models.CharField(max_length=50)
     slug = models.SlugField(max_length=200, unique=True, default='')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     updated_on = models.DateTimeField(auto_now=True)
