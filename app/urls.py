6213c411a9ba436e7ae0438748e78f8a0fc8da70
Diarmuid Sheeran <dssheeran@outlook.com>
2023-11-07 23:45:12 +0000

    Add Url path for error 500 page

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 from django.shortcuts import render
 from django.conf.urls import handler404
+from django.conf.urls import handler500
 
 urlpatterns = [
     path('', views.landing, name="landing"),
@@ -71,3 +72,8 @@ def custom_404(request, exception):
     return render(request, '404.html', status=404)
 
 handler404 = custom_404
+
+def custom_500(request):
+    return render(request, '500.html', status=500)
+
+handler500 = custom_500
